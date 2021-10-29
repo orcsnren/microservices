417d5ddac4989dcbfdfb9f29494a10da44ec64b3
Soner <sonereren39@gmail.com>
2021-10-29 16:44:56 +0300

    api gateway generated

--- currency-exchange-service/src/main/resources/data.sql
+++ currency-exchange-service/src/main/resources/data.sql
@@ -3,7 +3,7 @@ insert into currency_exchange
 values(10001,'USD','TR',10,'');
 insert into currency_exchange
 (id,currency_from,currency_to,conversion_multiple,environment)
-values(10002,'EUR','INR',11,'');
+values(10002,'EUR','TR',11,'');
 insert into currency_exchange
 (id,currency_from,currency_to,conversion_multiple,environment)
-values(10003,'AUD','INR',12,'');
+values(10003,'AUD','TR',12,'');
