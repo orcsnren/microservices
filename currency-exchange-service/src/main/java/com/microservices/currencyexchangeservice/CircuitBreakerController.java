19a6cda21d1398a463475c69f1c2a68e58e3d34f
sonereren <sonereren@localhost>
2021-10-31 05:02:10 +0300

    resilience4j examples

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,14 +1,35 @@
 package com.microservices.currencyexchangeservice;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 @RestController
 public class CircuitBreakerController {
 
+	private final Logger logger = LogManager.getLogger(CircuitBreakerController.class);
+
 	@GetMapping("sample-api")
+//	@Retry(name = "sample-api", fallbackMethod = "dummyResponse")
+//	@CircuitBreaker(name = "default", fallbackMethod = "dummyResponse")
+	@RateLimiter(name = "default")
+	//10s->10000 calls to this url
+	@Bulkhead(name="sample-api")//concurent call limit
 	public String api() {
-		return "Sample api";
+		logger.info("api call");
+//		ResponseEntity<String> entity = new RestTemplate().getForEntity("http://localhost:8080/dummy-url",
+//				String.class);
+//		return entity.getBody();
+		return "sample response";
+	}
+
+	public String dummyResponse(Exception ex) {
+		return "dummy response";
 	}
 
 }
